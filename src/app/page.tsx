2ea7c34557a4dc35371d1026bcffdcf3e4003904
antoniluthfi <luthfiantoni027@gmail.com>
2024-09-09 21:36:53 +0800

    restrructure files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import ExplorePrograms from "@/components/explore-programs/ExplorePrograms";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import OurServices from "@/components/our-services/OurServices";
-import RecentBlog from "@/components/recent-blog/RecentBlog";
-import Testimoni from "@/components/testimoni/Testimoni";
-import Tutors from "@/components/tutors/Tutors";
+import ExplorePrograms from "@/components/company-profile/explore-programs/ExplorePrograms";
+import Footer from "@/components/company-profile/Footer";
+import Hero from "@/components/company-profile/Hero";
+import Navbar from "@/components/company-profile/Navbar";
+import OurServices from "@/components/company-profile/our-services/OurServices";
+import RecentBlog from "@/components/company-profile/recent-blog/RecentBlog";
+import Testimoni from "@/components/company-profile/testimoni/Testimoni";
+import Tutors from "@/components/company-profile/tutors/Tutors";
 
 export default function Home() {
   return (
